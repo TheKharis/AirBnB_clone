c55ee778779c69aa79964c6c7d51807d4bac6e67
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2023-03-14 01:17:59 +0000

    Corrections to code task 3

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.state import State
-from models.review import Review
-from models.engine import file_storage
+"""Define initialization method for BaseModels"""
+
+from models.engine.file_storage import FileStorage
 
 
-storage = file_storage.FileStorage()
+storage = FileStorage()
 storage.reload()
