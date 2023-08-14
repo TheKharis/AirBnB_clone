deb357a8f2c3ddd25e63cda01e7d90c97a43178f
TheKharis <reubenagbenyegah86@gmail.com>
2023-08-14 21:47:16 +0000

    Task 3

---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        cdict = self.__dict__.copy()
+        cdict["__class__"] = self.__class__.__name__
+        cdict["created_at"] = self.created_at.isoformat()
+        cdict["updated_at"] = self.updated_at.isoformat()
+        return cdict
+
