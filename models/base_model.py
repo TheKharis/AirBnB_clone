9ac48ecb5ce3b4ea0fdb270d393b762ed5ed2233
TheKharis <reubenagbenyegah86@gmail.com>
2023-08-15 07:12:47 +0000

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,18 @@ from datetime import datetime
 
 class BaseModel():
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.fromisoformat(v);
+                else:
+                    self.__dict__[k] = v
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
@@ -23,4 +31,3 @@ class BaseModel():
         cdict["created_at"] = self.created_at.isoformat()
         cdict["updated_at"] = self.updated_at.isoformat()
         return cdict
-
